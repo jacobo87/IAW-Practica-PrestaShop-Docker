195afdf3a454d2260c4b6050d6a93dfbe39a0aef
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-02-21 15:05:32 +0100

    actualizados archivos

--- sql/database.sql
+++ sql/database.sql
@@ -3,12 +3,12 @@ CREATE DATABASE lamp_db CHARSET utf8mb4;
 USE lamp_db;
 
 CREATE TABLE users (
-    id int(11) NOT NULL auto_increment,
-    name varchar(100) NOT NULL,
-    age int(3) NOT NULL,
-    email varchar(100) NOT NULL,
-    PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  id int(11) NOT NULL auto_increment,
+  name varchar(100) NOT NULL,
+  age int(3) NOT NULL,
+  email varchar(100) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE USER IF NOT EXISTS 'lamp_user'@'%';
 SET PASSWORD FOR 'lamp_user'@'%' = 'lamp_password';
